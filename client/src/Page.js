aa00a9d804984c8c3e892f685e84186010d00dcc
yurimn <leeyou6757@gmail.com>
2022-05-05 03:35:27 +0900

    Update Page.js

--- client/src/Page.js
+++ client/src/Page.js
@@ -1,31 +1,26 @@
 import "./Page.css";
-import axios from 'axios'; //server와 통신
+import axios from "axios"; //server와 통신
 
-
-
-function Button({idd}) {
-	function getRandomLoc(){
+function Button({ idd }) {
+	function getRandomLoc() {
 		console.log("hi");
-		axios.get("/api/get_random_loc", {
-			method: 'GET',
-			params:{
-				id: document.getElementById('random-button').prop
-			}
-		})
-		.then((response) => {
-			console.log("success")
-			response.data.name 
-		})
+		axios
+			.get("/api/get_random_loc", {
+				method: "GET",
+				params: {
+					id: document.getElementById("random-button").prop,
+				},
+			})
+			.then((response) => {
+				console.log("success");
+				response.data.name;
+			});
 	}
 	return (
-		<button
-			id="random-button"
-			prop = "meal"
-			onClick={getRandomLoc}
-		>
+		<button id="random-button" prop="meal" onClick={getRandomLoc}>
 			idd
 		</button>
-	);	
+	);
 }
 
 function Page({ content }) {
