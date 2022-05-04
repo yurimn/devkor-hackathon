deef4e97c0a0fdf63539901794bd0bbee7a829a7
ulysseyson <ulyseyson57@gmail.com>
2022-05-05 03:42:50 +0900

    split button

--- client/src/Page.js
+++ client/src/Page.js
@@ -1,32 +1,6 @@
 import "./Page.css";
-import axios from 'axios'; //server와 통신
 
-
-
-function Button({idd}) {
-	function getRandomLoc(){
-		console.log("hi");
-		axios.get("/api/get_random_loc", {
-			method: 'GET',
-			params:{
-				id: document.getElementById('random-button').prop
-			}
-		})
-		.then((response) => {
-			console.log("success")
-			response.data.name 
-		})
-	}
-	return (
-		<button
-			id="random-button"
-			prop = "meal"
-			onClick={getRandomLoc}
-		>
-			idd
-		</button>
-	);	
-}
+import Button from './Button.js'
 
 function Page({ content }) {
 	return (
