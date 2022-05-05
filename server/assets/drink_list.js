8e6049d0da4cbca71e56a0cbec48d150abfc596d
ulysseyson <ulyseyson57@gmail.com>
2022-05-05 16:09:24 +0900

    list_json add

---
+++ server/assets/drink_list.js
@@ -0,0 +1,137 @@
+export default [
+    {
+        "name": "늘, 안암",
+        "explanation": "요리주점",
+        "location": "서울 성북구 고려대로24가길 13-2 1F"
+    },
+    {
+        "name": "주유소2",
+        "explanation": "요리주점",
+        "location": "서울 성북구 고려대로27길 19 2F"
+    },
+    {
+        "name": "히든트랙",
+        "explanation": "맥주, 호프",
+        "location": "서울 동대문구 약령시로 6"
+    },
+    {
+        "name": "다락",
+        "explanation": "요리주점",
+        "location": "서울 성북구 고려대로 95"
+    },
+    {
+        "name": "호랭이술집",
+        "explanation": "요리주점",
+        "location": "서울 성북구 고려대로26길 14 지하1층 B101호"
+    },
+    {
+        "name": "안암주점",
+        "explanation": "맥주, 호프",
+        "location": "서울 성북구 고려대로24길 11 지하층"
+    },
+    {
+        "name": "DaddyJ Hookah Bar",
+        "explanation": "BAR",
+        "location": "서울 성북구 고려대로24길 3-3 지하1층"
+    },
+    {
+        "name": "뜨락",
+        "explanation": "요리주점",
+        "location": "서울 성북구 고려대로24가길 11 1층"
+    },
+    {
+        "name": "두꺼비로맨스",
+        "explanation": "요리주점",
+        "location": "서울 성북구 고려대로26길 14 대륜빌딩 2층 201호 두꺼비로맨스 고대점"
+    },
+    {
+        "name": "별난주점",
+        "explanation": "맥주, 호프",
+        "location": "서울 성북구 고려대로24길 5"
+    },
+    {
+        "name": "호랑이술상 안암",
+        "explanation": "맥주, 호프",
+        "location": "서울 성북구 고려대로26길 55 2층"
+    },
+    {
+        "name": "아지트 안암",
+        "explanation": "BAR",
+        "location": "서울 성북구 고려대로24길 48 B1"
+    },
+    {
+        "name": "땡전집",
+        "explanation": "전통, 민속주점",
+        "location": "서울 성북구 고려대로24길 24"
+    },
+    {
+        "name": "동스꼬치",
+        "explanation": "오뎅, 꼬치",
+        "location": "서울 성북구 안암로 103"
+    },
+    {
+        "name": "팔레트 안암점",
+        "explanation": "와인",
+        "location": "서울 성북구 고려대로26길 42-4 1층 팔레트"
+    },
+    {
+        "name": "안암꼬치",
+        "explanation": "오뎅, 꼬치",
+        "location": "서울 성북구 안암로 85"
+    },
+    {
+        "name": "한잔의 추억",
+        "explanation": "요리주점",
+        "location": "서울 성북구 고려대로26길 30"
+    },
+    {
+        "name": "지금 보고싶다",
+        "explanation": "요리주점",
+        "location": "서울 성북구 고려대로26길 42-2 2층"
+    },
+    {
+        "name": "박군포차",
+        "explanation": "포장마차",
+        "location": "서울 성북구 고려대로26길 34"
+    },
+    {
+        "name": "88왕대포 고대본점",
+        "explanation": "포장마차",
+        "location": "서울 성북구 고려대로26길 49"
+    },
+    {
+        "name": "막걸리 찬가",
+        "explanation": "술집",
+        "location": "서울 성북구 고려대로24길 58-5 고려안암빌딩"
+    },
+    {
+        "name": "희야",
+        "explanation": "이자카야",
+        "location": "서울 성북구 고려대로26길 32 1F"
+    },
+    {
+        "name": "춘자",
+        "explanation": "포장마차",
+        "location": "서울 성북구 고려대로26길 45-4 1층or 지하1층"
+    },
+    {
+        "name": "번트",
+        "explanation": "와인",
+        "location": "서울 성북구 고려대로 19 1층 BURNT"
+    },
+    {
+        "name": "골목집",
+        "explanation": "요리주점",
+        "location": "서울 성북구 고려대로26길 33"
+    },
+    {
+        "name": "56st",
+        "explanation": "요리주점",
+        "location": "서울 성북구 고려대로26길 25 1층"
+    },
+    {
+        "name": "TUC",
+        "explanation": "BAR",
+        "location": "서울 동대문구 정릉천동로 155"
+    }
+];
