41ec96aeb21ff0204006af0e7a158609c8340186
yurimn <leeyou6757@gmail.com>
2022-05-05 09:41:51 +0900

    디자인

--- client/src/Main.js
+++ client/src/Main.js
@@ -3,7 +3,15 @@ import "./Main.css";
 function Main(props) {
 	return (
 		<div className="main">
-			<div className="main-title-text">KOREA UNIVERSITY</div>
+			<div className="main-text">
+				<div className="main-sub-text">
+					새내기를 위한 안암 가이드. 2022 ver
+				</div>
+				<div className="main-title-text">KOREA UNIVERSITY</div>
+			</div>
+			<div>
+				<img src={require("./pic/korea2.png").default} />
+			</div>
 		</div>
 	);
 }
--- server/routes/random.js
+++ server/routes/random.js
@@ -1,34 +1,27 @@
-import express  from 'express';//express import
+import express from "express"; //express import
 var router = express.Router();
 
-import fs from 'fs';//file 입출력을 위한 모듈 fs import
+import fs from "fs"; //file 입출력을 위한 모듈 fs import
 
-    router.get('/', function(req, res, next) {
-        res.render('index', { title: 'Express' });
-    });
+router.get("/", function (req, res, next) {
+	res.render("index", { title: "Express" });
+});
 
-    router.get('/api/get_random_loc', async(req, res) => {
-        try {
-            const id = req.query.id
-            var index = Math.floor(Math.random()*45+1)
-            // const idJsonFile = "./assets" + id + ".json"
-            const idJsonFile = "../../assets/meal_list.json"
-            fs.readFile(idJsonFile, 'utf-8', function(err, data){
-              var jsonData = JSON.parse(data)
+router.get("/api/get_random_loc", async (req, res) => {
+	const id = req.query.id;
+	var index = Math.floor(Math.random() * 45 + 1);
+	// const idJsonFile = "./assets" + id + ".json"
+	const idJsonFile = "../../assets/meal_list.json";
+	fs.readFile(idJsonFile, "utf-8", function (err, data) {
+		var jsonData = JSON.parse(data);
 
-              res.send({
-                // "name": obj.name,
-                // "explanation": obj.explanation,
-                // "location": obj.location,
-              })
-              
-                console.log(jsonData)
-            })
-        } catch(err) {
-            console.log(err);
-            res.send({
-                error: 'Can"t read api data'
-            });
-        }
-    });
+		res.send({
+			// "name": obj.name,
+			// "explanation": obj.explanation,
+			// "location": obj.location,
+		});
+
+		console.log(jsonData);
+	});
+});
 export default router;
